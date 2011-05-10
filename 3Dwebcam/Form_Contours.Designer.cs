c2c8270690b06691ad70fe1a99a4391c522afa1e
hamoriakos@gmail.com <hamoriakos@gmail.com@98d3b8fa-f3bc-ced4-534a-05ee682f4217>
2011-05-10 18:47:49 +0000

    legújabb

---
+++ 3Dwebcam/Form_Contours.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace _3Dwebcam
+{
+    partial class Form_Contours
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Location = new System.Drawing.Point(12, 12);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(320, 240);
+            this.panel.TabIndex = 0;
+            // 
+            // Form_Contours
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 264);
+            this.Controls.Add(this.panel);
+            this.Name = "Form_Contours";
+            this.Text = "Form_Contours";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Panel panel;
+
+    }
+}
